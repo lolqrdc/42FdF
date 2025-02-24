baa364ee68cc9fad83d8f8be844805cce35ef327
lolqrdc <lola-rdc@outlook.fr>
2025-02-24 21:18:21 +0100

    project done, added comments to explain some functions.

--- src/render/draw.c
+++ src/render/draw.c
@@ -3,15 +3,20 @@
 /*                                                        :::      ::::::::   */
 /*   draw.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: loribeir <loribeir@student.42.fr>          +#+  +:+       +#+        */
+/*   By: lolq <lolq@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 15:32:33 by loribeir          #+#    #+#             */
-/*   Updated: 2025/02/24 17:43:42 by loribeir         ###   ########.fr       */
+/*   Updated: 2025/02/24 21:14:53 by lolq             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
+/**
+ * @brief setting the color of a specific pixel, it will checks if the pixel
+ * coords are in the window boundaries. If valid, will calculate the memory
+ * addr of the pixel and set a color.
+ */
 void	put_pixel(t_fdf *fdf, int x, int y, int color)
 {
 	char	*tmp;
@@ -24,6 +29,11 @@ void	put_pixel(t_fdf *fdf, int x, int y, int color)
 	}
 }
 
+/**
+ * @brief init necessary params to do draw the line. It will calc
+ * the abs differences in x and y coords, determine the direction of 
+ * movement according to a start and end points. 
+ */
 void	init_line(t_line *line, t_point *first, t_point *end)
 {
 	line->dx = abs(end->screen_x - first->screen_x);
@@ -39,6 +49,11 @@ void	init_line(t_line *line, t_point *first, t_point *end)
 	line->error = line->dx + line->dy;
 }
 
+/**
+ * @brief implementation of the Bresenham's line algo. Enter in a loop
+ * that continues until the end point is reached. To each iteration it
+ * will draw the current pixel, update x and y coords. 
+ */
 void	draw_line(t_fdf *fdf, t_point *first, t_point *end)
 {
 	t_line	line;
@@ -65,6 +80,11 @@ void	draw_line(t_fdf *fdf, t_point *first, t_point *end)
 	}
 }
 
+/**
+ * @brief draw the entire map by connecting points with lines. 
+ * It will iterates through all points in the map. For each point,
+ * it draws a horizontal and vertical line to the next point (if it exist).
+ */
 void	draw_map(t_fdf *fdf)
 {
 	int	i;
